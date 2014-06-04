76157f0adf7e09d08999ae8d53acd3a0e4da0895
kamilwere <kamilwere@34dda854-b98c-4f2c-89c2-c5d403df0633>
2014-06-04 11:36:34 +0000

    git-svn-id: https://localhost:8443/svn/hml-2@3 34dda854-b98c-4f2c-89c2-c5d403df0633

--- trunk/src/dataMan/main.h
+++
@@ -1,5 +0,0 @@
-#ifndef main_h__
-#define main_h__
-
-
-#endif // main_h__
--- trunk/src/dataMan/trainingData.h
+++
@@ -1,7 +0,0 @@
-#ifndef trainingData_h__
-#define trainingData_h__
-#include "cv.h"
-#include "highgui.h"
-#include "cxcore.h"
-
-#endif // trainingData_h__
